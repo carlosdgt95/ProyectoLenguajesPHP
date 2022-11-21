56c47984390065a0532c21018c857dc87ba572a5
carlosdgt95 <cdgt9522@gmail.com>
2022-11-20 20:34:11 -0500

    codigo para logs

--- main.py
+++ main.py
@@ -1,7 +1,44 @@
+
 #import sintactico
 import ply.yacc as yacc
 from lexicophp import tokens
 
+import logging
+
+
+# Definición del logger root
+# -----------------------------------------------------------------------------
+logging.basicConfig(
+    format = '%(asctime)-5s %(name)-15s %(levelname)-8s %(message)s',
+    level  = logging.INFO,
+    filemode = "a"
+    )
+
+# Nuevos handlers
+# -----------------------------------------------------------------------------
+# Si el root logger ya tiene handlers, se eliminan antes de añadir los nuevos.
+# Esto es importante para que los logs no empiezen a duplicarse.
+if logging.getLogger('').hasHandlers():
+    logging.getLogger('').handlers.clear()
+    
+# Se añaden dos nuevos handlers al root logger, uno para los niveles de debug o
+# superiores y otro para que se muestre por pantalla los niveles de info o
+# superiores.
+file_debug_handler = logging.FileHandler('logs_debug.log')
+file_debug_handler.setLevel(logging.DEBUG)
+file_debug_format = logging.Formatter('%(asctime)-5s %(name)-15s %(levelname)-8s %(message)s')
+file_debug_handler.setFormatter(file_debug_format)
+logging.getLogger('').addHandler(file_debug_handler)
+
+consola_handler = logging.StreamHandler()
+consola_handler.setLevel(logging.INFO)
+consola_handler_format = logging.Formatter('%(asctime)-5s %(name)-15s %(levelname)-8s %(message)s')
+consola_handler.setFormatter(consola_handler_format)
+logging.getLogger('').addHandler(consola_handler)
+
+# ======================= MAIN SCRIPT ============================================
+logging.debug('Inicio main script')
+logging.info('Inicio main script')
 #def p_cuerpo(p):
 # '''cuerpo: salida
 #  | variable'''
@@ -73,4 +110,9 @@ while True:
     break
   if not s: continue
   validaRegla(s)
+
+logging.debug('Fin main script')
+logging.info('Fin main script')
+
+logging.shutdown()
 
